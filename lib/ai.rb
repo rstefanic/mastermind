1c8dd63c1ded6331b4efbc318d7685dba983cd0e
Robert Stefanic <rstefanic72@gmail.com>
2020-07-15 22:26:42 -0400

    Expanded AI decision making for building guesses

--- lib/ai.rb
+++ lib/ai.rb
@@ -32,9 +32,8 @@ module AI
     current_guess = Array.new(4)
     possible_colors = order_colors_by_confidence(possibilities)
 
-    puts possible_colors
     possible_colors.each do |possibility|
-      next if possibility.impossible?
+      next if possibility.impossible
 
       current_guess[possibility.position] = possibility if current_guess[possibility.position].nil?
     end
@@ -50,25 +49,23 @@ module AI
       end
     end
 
-
-    tries = 0
     # We never want to submit the same guess, so swap our least confidence guess
-    while previous_guesses.include?(current_guess)
-
+    if previous_guesses.include?(current_guess)
+      # highest_possibility = current_guess.max_by(&:confidence)
       lowest_possibility = current_guess.min_by(&:confidence)
       position_to_substitute = lowest_possibility.position
 
-      if tries > 4
-        position_to_substitute = (position_to_substitute + tries) % 4
-      end
-
       next_best_guesses = possibilities.select do |p|
-        p.position == position_to_substitute
+        p.position == position_to_substitute &&
+          p.color != lowest_possibility.color &&
+          !p.impossible
       end
 
-      random_substitution = next_best_guesses.sample
-      current_guess[position_to_substitute] = random_substitution
-      tries += 1
+      while previous_guesses.include?(current_guess) && next_best_guesses.count.positive?
+        random_substitution = next_best_guesses.sample
+        next_best_guesses.reject! { |guess| guess.color == random_substitution.color }
+        current_guess[position_to_substitute] = random_substitution
+      end
     end
 
     current_guess
@@ -89,39 +86,45 @@ module AI
     # If there were no hits from the last critique, then they cannot possibly be included in the final code
     if last_critique.empty?
       impossible_colors = last_guess.map(&:color)
-      adjusted_possibilities = possibilities.map do |possibility|
+      possibilities = possibilities.map do |possibility|
         # This color is impossible if our last guess came back as empty
         possibility.make_impossible if impossible_colors.include?(possibility.color)
         possibility
       end
-    # If any of them were correct, then depending on how many there were, we can greatly increase our confidence
-    elsif last_critique.include?(:correct)
-      lowest_possibility = last_guess.min_by(&:confidence)
-      if last_critique.count(:correct) == 3
-        adjusted_possibilities = possibilities.map do |possibility|
-          possibility.greatly_increase_confidence if last_guess.include?(possibility)
-          possibility.decrease_possibility if possibility == lowest_possibility
-          possibility
-        end
-      else
-        adjusted_possibilities = possibilities.map do |possibility|
-          possibility.slightly_increase_confidence if last_guess.include?(possibility)
-          possibility.decrease_possibility if possibility == lowest_possibility
-          possibility
-        end
+    end
+
+    # If any of them were correct, then we know that all of those colors are wrong
+    if last_critique.none?(:correct)
+      possibilities = possibilities.map do |possibility|
+        possibility.make_impossible if last_guess.include?(possibility)
+        possibility
       end
-    elsif last_critique.include?(:misplaced)
-      lowest_possibility = last_guess.min_by(&:confidence)
-      adjusted_possibilities = possibilities.map do |possibility|
-        last_guess.each do |guess|
-          possibility.slightly_increase_confidence if guess.color == possibility.color && guess.position != possibility.position
-        end
-        #possibility.decrease_possibility if possibility == lowest_possibility
+    # If 3 of them were correct, then we're creally close
+    elsif last_critique.count(:correct) == 3
+      possibilities = possibilities.map do |possibility|
+        possibility.greatly_increase_confidence if last_guess.include?(possibility)
+        possibility
+      end
+    else
+      possibilities = possibilities.map do |possibility|
+        possibility.slightly_increase_confidence if last_guess.include?(possibility)
         possibility
       end
     end
 
-    adjusted_possibilities
+    # If any of them were misplaced, then increase the confidence of those colors in all of the other positions
+    if last_critique.include?(:misplaced)
+      possibilities = possibilities.map do |possibility|
+        last_guess.each do |guess|
+          if guess.color == possibility.color && guess.position != possibility.position
+            possibility.slightly_increase_confidence
+          end
+        end
+        possibility
+      end
+    end
+
+    possibilities
   end
 
   def generate_possibilities
